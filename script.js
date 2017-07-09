17e8bbc6b7c01cabeb56b902cb3bfc3bde7f564b
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-07-09 14:14:40 -0600

    Fixed some CSS. Read complete, clearInputFields complete. Working on: validating enter button.

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 var title = $('.title').val;
 var url = $('.url').val;
 var enterBtn = $('.enter-btn').val;
-
 var readBtn = $('.read-btn').val;
 var deleteBtn = $('.delete.btn').val;
 
-$('.enter-btn').on('click', addCard);
+//click enter
+$('.enter-btn').on('click', addCard)
+               .on('click', clearInputFields);
 
-function addCard(e) {
+//  .on('click', validateInput);
+
+function clearInputFields() {
+  $('.title').val('');
+  $('.url').val('');
+}
+
+// function validateInput(e) {
+//   e.preventDefault();
+//   if (title !== '' || url !== '') {
+//     console.log('hellllllooo');
+//     alert('you may proceed');
+//   }
+// }
+
+function addCard(e, newCardTitle, newCardUrl) {
   e.preventDefault();
-  $('#website-list').append(`
+  $('#website-list').prepend(`
     <article>
       <h2 class="card-title">The Website Title</h2>
 
-      <a href="" target="_blank" class="card-url-link" >www.thewebsiteurl.com</a>
+      <a target="_blank" class="card-url-link" >www.thewebsiteurl.com</a>
 
       <p class="read-indicator">Read</p>
 
@@ -26,15 +42,26 @@ function addCard(e) {
   var newUrl = $('.url').val();
   $('.card-title').text(newWebsite);
   $('.card-url-link').text(newUrl);
-
-  // $('.title').text()
 }
 
-$('.read-btn').on('click', function () {
-  readBtn.style.color = '#f05a28';
-  readBtn.style.fontWeight = '600';
-  readBtn.style.borderBottom = 'none';
+$('#website-list').on('click', '.card-url-link', function (e) {
+    $(e.target).parent().find('.read-indicator').addClass('indicator-on');
+  });
+
+//click on link in card
+$('.card-url-link').on('click', function () {
+  console.log('hllo');
 });
 
+// function indicatorOn() {
+//   console.log('hello');
+//   $('.read-indicator').addClass('indicator-on');
+// }
+
+//need to write a function to delete card
 $('.delete-btn').on('click', function () {
 });
+
+// $(document).ready(function () {
+//   console.log('is this working');
+// });
