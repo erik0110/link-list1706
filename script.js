8f93421d34ff183652a907223ce4ea1c2ecde6df
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-07-10 08:48:50 -0600

    enables enter button upon typing. Amy ES6 doesn't work.

--- script.js
+++ script.js
@@ -1,35 +1,52 @@
-var title = $('.title').val;
-var url = $('.url').val;
-var enterBtn = $('.enter-btn').val;
-var readBtn = $('.read-btn').val;
-var deleteBtn = $('.delete.btn').val;
+// jshint esversion: 6
 
-//click enter
-$('.enter-btn').on('click', addCard)
-               .on('click', clearInputFields);
+var $title = $('.title');
+var $url = $('.url');
+var $enterBtn = $('.enter-btn');
+var $readBtn = $('.read-btn');
+var $deleteBtn = $('.delete.btn');
+var $newCard = $('article');
 
-//  .on('click', validateInput);
+//click enter and it validates input, clears input fields of form
+$('.enter-btn').on('click', function () {
+  clearInputFields();
+  addCard();
+});
 
+//this clears the input fields
 function clearInputFields() {
   $('.title').val('');
   $('.url').val('');
 }
 
-// function validateInput(e) {
-//   e.preventDefault();
-//   if (title !== '' || url !== '') {
-//     console.log('hellllllooo');
-//     alert('you may proceed');
-//   }
-// }
+$url.on('input', function () {
+  validateInput();
+});
 
-function addCard(e, newCardTitle, newCardUrl) {
-  e.preventDefault();
+$title.on('input', function () {
+  validateInput();
+});
+
+//make sure user typed something in, once they do it will
+//run addCard- STILL THINKS THE VALUE IS UNDEFINED
+function validateInput() {
+  $title = $('.title').val();
+  $url = $('.url').val();
+  if ($title.length === 0 || $url.length === 0) {
+    $enterBtn.attr('disabled', true);
+  } else {
+    $enterBtn.attr('disabled', false);
+  }
+}
+
+function addCard() {
+  var $newWebsite = $('.title').val;
+  var $newUrl = $('.url').val;
   $('#website-list').prepend(`
     <article>
-      <h2 class="card-title">The Website Title</h2>
+      <h2 class="card-title">${$newWebsite}</h2>
 
-      <a target="_blank" class="card-url-link" >www.thewebsiteurl.com</a>
+      <a href="${$newUrl}" target="_blank" class="card-url-link" >${$newUrl}</a>
 
       <p class="read-indicator">Read</p>
 
@@ -37,31 +54,22 @@ function addCard(e, newCardTitle, newCardUrl) {
   </article>
   `);
 
-  //take user input --> card h2 and p
-  var newWebsite = $('.title').val();
-  var newUrl = $('.url').val();
-  $('.card-title').text(newWebsite);
-  $('.card-url-link').text(newUrl);
+  //this takes user input and print into card h2 and p
+  // $('.card-title').text(newWebsite);
+  // $('.card-url-link').text(newUrl);
 }
 
+//this targets the link for a click so the read words turn red
 $('#website-list').on('click', '.card-url-link', function (e) {
     $(e.target).parent().find('.read-indicator').addClass('indicator-on');
+    $('.card-url-link').attr('href', newUrl);
   });
-
-//click on link in card
-$('.card-url-link').on('click', function () {
-  console.log('hllo');
-});
-
-// function indicatorOn() {
-//   console.log('hello');
-//   $('.read-indicator').addClass('indicator-on');
-// }
 
 //need to write a function to delete card
 $('.delete-btn').on('click', function () {
 });
 
-// $(document).ready(function () {
-//   console.log('is this working');
-// });
+// $('#user-input').validate(); {
+//   required: true;
+//   url: true;
+// };
